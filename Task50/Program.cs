149715771567de10a453c9962c5588df31d33548
TayaL <tayalyapina555@gmail.com>
2022-12-05 05:51:25 +0300

    Files task 47, task 52, task 50 and task 50 var 2 added

---
+++ Task50/Program.cs
@@ -0,0 +1,66 @@
+﻿/*Задача 50. Напишите программу, которая на вход принимает позиции элемента 
+в двумерном массиве, и возвращает значение этого элемента или же указание, 
+что такого элемента нет.
+
+Например, задан массив:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+
+17 -> такого числа в массиве нет
+*/
+
+int rows = ReadInt("Введите количество строк: ");
+int columns = ReadInt("Введите количество столбцов: ");
+int[,] numbers = new int[rows, columns];
+FillMatrixRandomNumbers(numbers);
+WriteMatrix(numbers);
+
+Console.Write("Введите позицию проверяемого элемента в двумерном массиве: ");
+string? output = Console.ReadLine();
+
+int digit = Convert.ToInt32 (output);
+
+
+Console.Write($"{digit} -> ");
+
+int firstDigit = digit/10;
+int secondDigit = digit%10;
+
+
+if(firstDigit <= numbers.GetLength(0) && secondDigit <= numbers.GetLength(1))
+{
+    Console.Write(numbers[firstDigit, secondDigit]);   
+}
+else       
+Console.Write($"Такого числа в массиве нет");
+
+void FillMatrixRandomNumbers(int[,] array) 
+{
+    for(int i = 0; i < array.GetLength(0); i++)
+    {
+        for(int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(1, 10);
+        }
+    }
+}
+
+void WriteMatrix(int[,] array)  
+{
+    for(int i = 0; i < array.GetLength(0); i++)
+    {
+        for(int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
+
+int ReadInt(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
---
+++ Task50Var2/Program.cs
@@ -0,0 +1,63 @@
+﻿/*Задача 50. Напишите программу, которая на вход принимает позиции элемента 
+в двумерном массиве, и возвращает значение этого элемента или же указание, 
+что такого элемента нет.
+
+Например, задан массив:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+
+17 -> такого числа в массиве нет
+*/
+
+int rows = ReadInt("Введите количество строк: ");
+int columns = ReadInt("Введите количество столбцов: ");
+int[,] numbers = new int[rows, columns];
+FillMatrixRandomNumbers(numbers);
+WriteMatrix(numbers);
+
+Console.WriteLine("Введите позицию проверяемого элемента в двумерном массиве");
+
+int rowsNumber = ReadInt("Номер строки: ");
+int columnsNumber = ReadInt("Номер столбца: ");
+
+Console.Write($"{rowsNumber}{columnsNumber} -> ");
+
+if(rowsNumber <= numbers.GetLength(0) && columnsNumber <= numbers.GetLength(1))
+{
+    Console.Write(numbers[rowsNumber, columnsNumber]);   
+}
+
+else       
+    Console.Write($"Такого числа в массиве нет");
+
+
+void FillMatrixRandomNumbers(int[,] array) 
+{
+    for(int i = 0; i < array.GetLength(0); i++)
+    {
+        for(int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(1, 10);
+        }
+    }
+}
+
+void WriteMatrix(int[,] array)  
+{
+    for(int i = 0; i < array.GetLength(0); i++)
+    {
+        for(int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
+
+int ReadInt(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
